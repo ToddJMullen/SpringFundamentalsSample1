f89f60a6d3e239d2b3b3de02706e2cd26b9f4efc
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-01-20 00:18:03 -0500

    Abstract CustomerService interface from CustomerServiceImpl

---
+++ src/main/java/com/ps/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.ps.service;
+
+import java.util.List;
+
+import com.ps.model.Customer;
+
+public interface CustomerService {
+
+	List<Customer> findAll();
+
+}
--- src/main/java/com/ps/service/CustomerServiceImpl.java
+++ src/main/java/com/ps/service/CustomerServiceImpl.java
@@ -6,11 +6,15 @@ import com.ps.model.Customer;
 import com.ps.repository.CustomerRepository;
 import com.ps.repository.HibernateCustomerRepositoryImpl;
 
-public class CustomerServiceImpl {
+public class CustomerServiceImpl implements CustomerService {
 
 	
 	private CustomerRepository customerRepo = new HibernateCustomerRepositoryImpl();
 			
+	/* (non-Javadoc)
+	 * @see com.ps.service.CustomerService#findAll()
+	 */
+	@Override
 	public List<Customer> findAll(){
 		return customerRepo.findAll();
 	}
