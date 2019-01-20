0d7d826759ce859ac7b40c4d20a45228e7ff51ac
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-01-19 23:41:28 -0500

    Add base Customer class

---
+++ src/main/java/com/ps/model/Customer.java
@@ -0,0 +1,30 @@
+package com.ps.model;
+
+public class Customer {
+
+	private String nameFirst;
+	private String nameLast;
+	
+	
+//	public void Customer() {
+//		//this is what the default constructor used when none is defined
+//	}
+	
+
+	public String getNameFirst() {
+		return nameFirst;
+	}
+
+	public void setNameFirst(String nameFirst) {
+		this.nameFirst = nameFirst;
+	}
+
+	public String getNameLast() {
+		return nameLast;
+	}
+
+	public void setNameLast(String nameLast) {
+		this.nameLast = nameLast;
+	}
+
+}
