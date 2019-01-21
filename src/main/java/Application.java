52c96852f769714447ef7cacf527bbd8e352df7f
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-01-21 17:26:15 -0500

    Setup autowiring of service & repo class instances - Add applicationContext.xml with added context NS to provide the annotation-config & component-scan tags needed - CustomerServiceImpl.java, Add @Service('customerService') annotation to mark class as a provided -- Add @Autowired to customerRepo var to mark as consumer of the same - HibernateCustomerRepositoryImpl.java, Add @Repository('customerRepo') to mark as provider to an autowired instance of the same name.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,6 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.ps.service.CustomerService;
 import com.ps.service.CustomerServiceImpl;
 
@@ -5,7 +8,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		
-		CustomerService service = new CustomerServiceImpl();
+//		CustomerService service = new CustomerServiceImpl();
+		
+		ApplicationContext appCtx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		CustomerService service = appCtx.getBean("customerService", CustomerService.class );
 		
 		System.out.println( service.findAll().get(0).getNameFirst() );
 
--- src/main/java/com/ps/service/CustomerServiceImpl.java
+++ src/main/java/com/ps/service/CustomerServiceImpl.java
@@ -2,14 +2,24 @@ package com.ps.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.ps.model.Customer;
 import com.ps.repository.CustomerRepository;
-import com.ps.repository.HibernateCustomerRepositoryImpl;
+//import com.ps.repository.HibernateCustomerRepositoryImpl;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
+	@Autowired
+	private CustomerRepository customerRepo;//	 = new HibernateCustomerRepositoryImpl();
+	//note that this is still using the hard wired dependency bc this rev was started
+	//on the base started sample (rev 1) "spring_sample" & hence the "poor" example code is the start
+	//& applicationContext IS NOT wired to use constructor injection or setter injection
+	//to provide the CustomerRepository instance that should be used (as in rev 2 "spring_sample_xml").
+	//In rev 3, we will use autowiring/annotations to decouple the Repo instance as the preferred method
 	
-	private CustomerRepository customerRepo = new HibernateCustomerRepositoryImpl();
 			
 	/* (non-Javadoc)
 	 * @see com.ps.service.CustomerService#findAll()
