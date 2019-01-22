24acbde87a79e197cedbdc6998a5e7be72de9670
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-01-21 20:06:16 -0500

    Disable class level DI auto wiring of CustomerServiceImpl - Add member level DI wiring to provide the CustomerRepo instance to be used

--- src/main/java/com/ps/service/CustomerServiceImpl.java
+++ src/main/java/com/ps/service/CustomerServiceImpl.java
@@ -12,14 +12,23 @@ import com.ps.repository.CustomerRepository;
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-	@Autowired
+//	@Autowired
 	private CustomerRepository customerRepo;//	 = new HibernateCustomerRepositoryImpl();
 	//note that this is still using the hard wired dependency bc this rev was started
 	//on the base started sample (rev 1) "spring_sample" & hence the "poor" example code is the start
 	//& applicationContext IS NOT wired to use constructor injection or setter injection
 	//to provide the CustomerRepository instance that should be used (as in rev 2 "spring_sample_xml").
 	//In rev 3, we will use autowiring/annotations to decouple the Repo instance as the preferred method
-			
+	
+	@Autowired
+	public void setCustomerRepo(CustomerRepository customerRepo) {
+		System.out.println("setCutomerRepo() called for setter injection with " + customerRepo.getClass() );
+		this.customerRepo = customerRepo;
+	}
+	//this ^^^ member injection method of configuring autowiring is basically the same as would be done w/o Spring
+	//both methods can be used & may be considered a matter of preference
+	//by providing this explicit setter with the @Autowired, Spring knows that DI is being requested by type here
+
 	/* (non-Javadoc)
 	 * @see com.ps.service.CustomerService#findAll()
 	 */
