cee39e87bfb32c5e23f40c352ec738383cded2ee
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-01-25 22:58:13 -0500

    Replace XML initial values being set by tag with @Value('${prop name}') style declaration

--- src/main/java/com/ps/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/ps/repository/HibernateCustomerRepositoryImpl.java
@@ -3,15 +3,28 @@ package com.ps.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.ps.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 	
+	@Value("${dbUsername}")
+	private String dbUsername;
+	
+	//Commented out when switched to annotation config import
+//	public void setDbUsername(String dbUsername) {
+//		this.dbUsername = dbUsername;
+//	}
+
 	/* (non-Javadoc)
 	 * @see com.ps.repository.CustomerRepository#findAll()
 	 */
 	@Override
 	public List<Customer> findAll(){
+		
+		System.out.println("HibernateCustomerRepositoryImpl().findAll()\ndbUsername: " + dbUsername );
+		
 		List<Customer> customerList = new ArrayList<Customer>();
 		
 		Customer cust = new Customer();
