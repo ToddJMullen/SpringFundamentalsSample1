f93b4c5844b4f5c5ee5662263f92c69c71a91fe3
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-01-25 23:47:27 -0500

    Try to add @Value() defaults for class props given in app.properties. - Application runs, but the values are not being read or set. - Abandoning as fruitless and in favor of moving forward

--- src/main/java/com/ps/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/ps/repository/HibernateCustomerRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.ps.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import com.ps.model.Customer;
@@ -10,12 +11,25 @@ import com.ps.model.Customer;
 @Repository("customerRepository")//<< stereotype annotation identifies this repo bean
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 	
+	@Value("${appSecret1}")
+	public String dbUsername;
+	
+	@Value("${appSecret2}")
+	private String dbPassword;
+	
+	//No error, but values are not being set.
+	//Can't figure out why
+	
+	
 	/* (non-Javadoc)
 	 * @see com.ps.repository.CustomerRepository#findAll()
 	 */
 	@Override
 	public List<Customer> findAll(){
 		List<Customer> customerList = new ArrayList<Customer>();
+		
+		System.out.println("...findAll() dbUsername:" + dbUsername );
+		System.out.println("...findAll() dbPassword:" + dbPassword );
 		
 		Customer cust = new Customer();
 		cust.setNameFirst("Todd");
